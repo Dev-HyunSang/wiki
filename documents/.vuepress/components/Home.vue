4873f9858a59545c5e9c6e5158bcade6db51d484
Dev-HyunSang <me@hyunsang.dev>
2022-12-29 19:27:12 +0900

    docs: update docs

--- documents/.vuepress/components/Home.vue
+++ documents/.vuepress/components/Home.vue
@@ -5,18 +5,48 @@
       <li>하루에 하나씩 공부하면서 공부한 내용들을 기록합니다.</li>
       <li>Go언어와 Rust를 항상 공부하며 개발하고 있습니다.<br/>프로그래밍 언어를 공부하면서 배운 다양한 내용들을 정리합니다.</li>
       <li>항상 프로그래밍 언어를 배우며, 컴퓨터 공학적인 내용에 대해서 관심이 많습니다.<br/>주로 메모리와 데이터 구조에 대해서 공부하고 있습니다.</li>
-      <li><b>본 페이지는 <a href="https://github.com/milooy/TIL">milooy/TIL</a> 테마를 사용하여 GitHub Page 기반으로 동작하고 있습니다.</b></li>
+    </ul>
+    <h2>2023년 목표</h2>
+    <ul>
+      <li>최소 국내 2곳, 해외 1곳 이상 여행하기(서울 제외)</li>
+      <li>gRPC, Kubernetes 공부하기</li>
+      <li>인문학 관련 서적 5권 이상 읽기</li>
+      <li>프리드리히 니체의 '차라투스트라는 이렇게 말했다' 완독하기</li>
+      <li>다양한 분들과 다양한 곳에서 커피챗 50명 채우기(오프라인, 온라인)</li>
+      <li>오프라인 혹은 온라인에서 Go언어 관련 발표하기</li>
     </ul>
     <blockquote>
       <b>”모든 것의 시작은 위험하다. 그러나 무엇을 막론하고,<br/>
       시작하지 않으면 아무것도 시작되지 않는다.“</b><br/>
       프리드리히 니체의 "인간적인 너무나 인간적인"에서
     </blockquote>
+    <hr/>
+    <div class="links">
+        <li class="li-item-none">
+          <a href="https://hyunsang.dev">Tech Blog</a>
+        </li>
+        <li class="li-item-none">
+          <a href="https://about.hyunsang.dev">Portfolio</a>
+        </li>
+        <li class="li-item-none">
+          <a href="https://github.com/dev-hyunsang">GitHub</a>
+        </li>
+        <li class="li-item-none">
+          <a href="https://www.facebook.com/hyun.sang.0625">Facebook</a>
+        </li>
+    </div>
   </div>
 </template>
 <style>
   blockquote {
     border-left: 3px solid #3eaf7c;
     padding-left: 10px;
+  }
+  .links {
+    text-align: right;
+  }
+  .li-item-none {
+    list-style: none;
+    padding-bottom: 0.1rem;
   }
 </style>
