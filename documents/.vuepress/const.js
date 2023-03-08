fd94d6804a1373c05b8025974c6a014a81f2865e
HyunSang Park <me@hyunsang.dev>
2023-03-09 00:31:34 +0900

    docs: update content

--- documents/.vuepress/const.js
+++ documents/.vuepress/const.js
@@ -6,7 +6,12 @@ exports.Golang = [
   "Golang/20221229-JWT에서-Redis를-사용하는-이유",
   "Golang/20230107-Livekit-SDK-with-Golang",
   "Golang/20230122-환경변수에-대한-고찰",
-  "Golang/20230208-네이버-클라우드-플랫폼과-AWS-SDK-for-Go"
+  "Golang/20230208-네이버-클라우드-플랫폼과-AWS-SDK-for-Go",
+  "Golang/20230220-Go-package-version"
+]
+
+exports.AlgorithmWithGolang = [
+  "Algorithm-with-Golang/programmers"
 ]
 
 exports.Rust = [
