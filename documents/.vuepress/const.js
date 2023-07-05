2f52909988aa0094b36e53fd448ea33afd5084f8
HyunSang Park <me@hyunsang.dev>
2023-07-05 10:45:18 +0900

    docs: update docs and changed setting

--- documents/.vuepress/const.js
+++ documents/.vuepress/const.js
@@ -1,25 +1,25 @@
 exports.Golang = [
-  "Golang/20221217-변수와-자료형에-대해",
-  "Golang/20221219-자료-구조",
-  "Golang/20221225-고루틴과-채널",
-  "Golang/20221226-Request-and-Response-Format",
-  "Golang/20221229-JWT에서-Redis를-사용하는-이유",
-  "Golang/20230107-Livekit-SDK-with-Golang",
-  "Golang/20230122-환경변수에-대한-고찰",
-  "Golang/20230208-네이버-클라우드-플랫폼과-AWS-SDK-for-Go",
-  "Golang/20230403-Bazel-with-Golang",
+  "golang/20221217-변수와-자료형에-대해",
+  "golang/20221219-자료-구조",
+  "golang/20221225-고루틴과-채널",
+  "golang/20221226-Request-and-Response-Format",
+  "golang/20221229-JWT에서-Redis를-사용하는-이유",
+  "golang/20230107-Livekit-SDK-with-Golang",
+  "golang/20230122-환경변수에-대한-고찰",
+  "golang/20230208-네이버-클라우드-플랫폼과-AWS-SDK-for-Go",
+  "golang/20230403-Bazel-with-Golang",
 ]
 
 exports.AlgorithmWithGolang = [
-  "Algorithm-with-Golang/programmers",
-  "Algorithm-with-Golang/20230525-what-is-dfs",
-  "Algorithm-with-Golang/20230523-data-structure-and-algorithms-1",
+  "algorithm-with-golang/programmers",
+  "algorithm-with-golang/20230525-what-is-dfs",
+  "algorithm-with-golang/20230523-data-structure-and-algorithms-1",
 ]
 
 exports.ComputerScience = [
-  "Computer-Science/20230427-what-is-kernel",
-  "Computer-Science/20230521-what-is-fota-and-ota",
-  "Computer-Science/20230526-what-is-ca"
+  "computer-science/20230427-what-is-kernel",
+  "computer-science/20230521-what-is-fota-and-ota",
+  "computer-science/20230526-what-is-ca"
 ]
 
 exports.CodingInterviewUniversity = [
@@ -27,26 +27,26 @@ exports.CodingInterviewUniversity = [
 ]
 
 exports.Rust = [
-  "Rust/rust-building-application",
-  "Rust/20230517-Rust-Ownership"
+  "rust/rust-building-application",
+  "rust/20230517-Rust-Ownership"
 ]
 
 exports.Media = [
-  "Media/TimeLapse-with-FFmpeg"
+  "media/TimeLapse-with-FFmpeg"
 ]
 exports.Security = [
-  "Security/2023",
-  "Security/cryptology",
+  "security/2023",
+  "security/cryptology",
 ]
 
 exports.References = [
-  "References/Golang",
-  "References/Rust",
-  "References/DevOps",
-  "References/Security",
-  "References/Media",
-  "References/Organizational-Culture",
-  "References/ETC"
+  "references/Golang",
+  "references/Rust",
+  "references/DevOps",
+  "references/Security",
+  "references/Media",
+  "references/Organizational-Culture",
+  "references/ETC"
 ]
 
 exports.Thinkgings = [
@@ -54,23 +54,23 @@ exports.Thinkgings = [
 ]
 
 exports.Errors = [
-  "Errors/GitHub",
-  "Errors/Golang",
-  "Errors/DataBase"
+  "errors/GitHub",
+  "errors/Golang",
+  "errors/DataBase"
 ]
 
 exports.BooksList = [
-  "Books/20221217-차라투스트라는-이렇게-말했다",
-  "Books/20221218-니체의-지혜",
-  "Books/20221218-어떻게-살-것인가",
-  "Books/20230110-니체와-장자는-이렇게-말했다",
-  "Books/20230404-나의-첫-죽음학-수업"
+  "books/20221217-차라투스트라는-이렇게-말했다",
+  "books/20221218-니체의-지혜",
+  "books/20221218-어떻게-살-것인가",
+  "books/20230110-니체와-장자는-이렇게-말했다",
+  "books/20230404-나의-첫-죽음학-수업"
 ];
 
 exports.Newtwork = [
-  "Network/20230417-TCP"
+  "networks/20230417-TCP"
 ]
 
 exports.Videos = [
-  "Videos/Experience-and-Advice"
+  "videos/Experience-and-Advice"
 ]
---
+++ documents/References/database.md
@@ -0,0 +1,9 @@
+# DataBase
+
+## 데이터베이스가 뭔가요?
+- [개발시 데이터베이스 선택 가이드 (총정리)](https://youtu.be/ZVuHZ2Fjkl4)
+
+## ACID & Transaction
+- [[DB] Transaction 과 ACID란?](https://chrisjune-13837.medium.com/db-transaction-%EA%B3%BC-acid%EB%9E%80-45a785403f9e)
+- [[DB] Transaction 과 ACID 란 무엇인가?](https://blog.yevgnenll.me/posts/what-is-acid-about-transaction)
+- [[데이터베이스] 트랜잭션의 ACID 성질](https://hanamon.kr/%EB%8D%B0%EC%9D%B4%ED%84%B0%EB%B2%A0%EC%9D%B4%EC%8A%A4-%ED%8A%B8%EB%9E%9C%EC%9E%AD%EC%85%98%EC%9D%98-acid-%EC%84%B1%EC%A7%88/)
